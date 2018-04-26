4bfa6ac5a92770359cbf676698d532e8dd44279a
Max Baun <max@d3applications.com>
2018-04-26 14:21:54 -0400

    removing license info

--- docroot/wp-content/themes/autosound/functions.php
+++ docroot/wp-content/themes/autosound/functions.php
@@ -41,6 +41,3 @@ define('DISALLOW_FILE_EDIT', true); // Don't allow file edtiting
 define('DISALLOW_FILE_MODS', true); // Don't allow plugin uploads
 
 add_filter('wpcf7_load_css', '__return_false');
-
-define('WPMDB_LICENCE', 'b6fef406-3d49-4d5a-b7b0-098b1914b2f3');
-define('AS3CFPRO_LICENCE', 'bad1437c-d20d-49c8-8767-f1695926cc39');
