9bca234fe599f69deaac11adb069b19523caffa2
Max Baun <max@d3applications.com>
2018-05-08 12:04:23 -0400

    cookie build

--- docroot/wp-content/themes/autosound/tools/webpack.config.prod.js
+++ docroot/wp-content/themes/autosound/tools/webpack.config.prod.js
@@ -2,26 +2,26 @@ var webpack = require('webpack');
 var config = require('./webpack.config.js');
 var Compress = require('compression-webpack-plugin');
 
-// config.plugins.push(
-// 	new webpack.LoaderOptionsPlugin({
-// 		minimize: true,
-// 		debug: false
-// 	}),
-// 	new webpack.DefinePlugin({
-// 		'process.env': {
-// 			NODE_ENV: JSON.stringify('production')
-// 		}
-// 	}),
-// 	new webpack.optimize.AggressiveMergingPlugin(),
-// 	new Compress({
-// 		asset: '[path].gz[query]',
-// 		algorithm: 'gzip',
-// 		test: /\.js$|\.css$/,
-// 		threshold: 10240,
-// 		minRatio: 0.8
-// 	})
-// );
-//
-// config.optimization.minimize = true;
+config.plugins.push(
+	new webpack.LoaderOptionsPlugin({
+		minimize: true,
+		debug: false
+	}),
+	new webpack.DefinePlugin({
+		'process.env': {
+			NODE_ENV: JSON.stringify('production')
+		}
+	}),
+	new webpack.optimize.AggressiveMergingPlugin(),
+	new Compress({
+		asset: '[path].gz[query]',
+		algorithm: 'gzip',
+		test: /\.js$|\.css$/,
+		threshold: 10240,
+		minRatio: 0.8
+	})
+);
+
+config.optimization.minimize = true;
 
 module.exports = config;
