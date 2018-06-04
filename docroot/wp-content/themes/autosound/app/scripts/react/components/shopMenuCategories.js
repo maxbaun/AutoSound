03c3d0e0ae431b2c5d9972eee644526b3307e721
Max Baun <max@d3applications.com>
2018-06-04 19:46:14 -0400

    adding support for subcategories in shop

--- docroot/wp-content/themes/autosound/app/scripts/react/components/shopMenuCategories.js
+++ docroot/wp-content/themes/autosound/app/scripts/react/components/shopMenuCategories.js
@@ -4,15 +4,21 @@ import * as ImmutablePropTypes from 'react-immutable-proptypes';
 import {Map, List} from 'immutable';
 import {Link} from 'react-router-dom';
 
-import {unique, noop} from '../utils/componentHelpers';
+import {unique, noop, click} from '../utils/componentHelpers';
 import Placeholder from './placeholder';
 
 export default class ShopMenuCategories extends Component {
 	constructor(props) {
 		super(props);
 
+		this.state = {
+			catDropdown: 0
+		};
+
 		this.renderDefaultCategories = ::this.renderDefaultCategories;
 		this.renderCategories = ::this.renderCategories;
+		this.renderSubcategories = ::this.renderSubcategories;
+		this.handleToggleClick = ::this.handleToggleClick;
 
 		this.fetch = unique();
 	}
@@ -21,12 +27,20 @@ export default class ShopMenuCategories extends Component {
 		actions: PropTypes.objectOf(PropTypes.func),
 		filters: ImmutablePropTypes.map,
 		loading: PropTypes.bool
-	}
+	};
 
 	static defaultProps = {
 		actions: {noop},
 		filters: Map(),
 		loading: true
+	};
+
+	handleToggleClick(catDropdown) {
+		if (this.state.catDropdown === catDropdown) {
+			catDropdown = 0;
+		}
+
+		this.setState({catDropdown});
 	}
 
 	render() {
@@ -35,9 +49,7 @@ export default class ShopMenuCategories extends Component {
 		return (
 			<div className="shop-menu__group">
 				<h5>Categories</h5>
-				<ul>
-					{loading ? this.renderDefaultCategories() : this.renderCategories()}
-				</ul>
+				<ul>{loading ? this.renderDefaultCategories() : this.renderCategories()}</ul>
 			</div>
 		);
 	}
@@ -73,12 +85,50 @@ export default class ShopMenuCategories extends Component {
 					<Link to="/">All</Link>
 				</li>
 				{filters.get('categories').map(c => {
+					if (c.get('parent')) {
+						return null;
+					}
+
 					return (
 						<li key={c.get('id')}>
 							<Link to={c.get('link')}>{c.get('title')}</Link>
+							{this.renderSubcategories(c.get('id'))}
 						</li>
 					);
 				})}
+			</Fragment>
+		);
+	}
+
+	renderSubcategories(catId) {
+		const {filters} = this.props;
+		const {catDropdown} = this.state;
+		const subCategories = filters.get('categories').filter(c => c.get('parent') === catId);
+
+		if (!subCategories || subCategories.count() === 0) {
+			return null;
+		}
+
+		const wrapClass = ['shop-menu__dropdown'];
+
+		if (catId === catDropdown) {
+			wrapClass.push(['active']);
+		}
+
+		return (
+			<Fragment>
+				<span className="shop-menu__dropdown-toggle">
+					<span onClick={click(this.handleToggleClick, catId)} className="fa fa-angle-down"/>
+				</span>
+				<ul className={wrapClass.join(' ')}>
+					{subCategories.map(c => {
+						return (
+							<li key={c.get('id')}>
+								<Link to={c.get('link')}>{c.get('title')}</Link>
+							</li>
+						);
+					})}
+				</ul>
 			</Fragment>
 		);
 	}
