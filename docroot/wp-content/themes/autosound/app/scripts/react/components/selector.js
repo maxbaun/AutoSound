23c2a8ea9c7a7a67072603a6e18fe4d32a15af84
Max Baun <max@d3applications.com>
2018-06-04 20:18:32 -0400

    fixing header top block dropdown bug for touch devices

--- docroot/wp-content/themes/autosound/app/scripts/react/components/selector.js
+++ docroot/wp-content/themes/autosound/app/scripts/react/components/selector.js
@@ -13,24 +13,30 @@ export default class Selector extends Component {
 		this.setWidth = ::this.setWidth;
 		this.renderMobile = ::this.renderMobile;
 		this.handleLocationUpdate = ::this.handleLocationUpdate;
+		this.handleStoreBlockToggle = ::this.handleStoreBlockToggle;
 
 		this.state = {
 			location: this.getSavedLocation(),
 			maxWidth: 0,
-			windowWidth: window.innerWidth
+			windowWidth: window.innerWidth,
+			storeActive: false
 		};
 	}
 
 	static propTypes = {
 		locations: ImmutablePropTypes.list
-	}
+	};
 
 	static defaultProps = {
 		locations: List()
-	}
+	};
 
 	componentDidMount() {
 		this.setWidth();
+
+		this.setState({
+			location: this.getSavedLocation()
+		});
 
 		window.addEventListener('resize', this.setWidth);
 	}
@@ -64,8 +70,12 @@ export default class Selector extends Component {
 		});
 	}
 
+	handleStoreBlockToggle(storeActive) {
+		this.setState({storeActive});
+	}
+
 	render() {
-		const {maxWidth, windowWidth, location: currentLocation} = this.state;
+		const {maxWidth, windowWidth, location: currentLocation, storeActive} = this.state;
 		const {locations} = this.props;
 
 		if (windowWidth < responsive.collapse) {
@@ -74,7 +84,12 @@ export default class Selector extends Component {
 
 		return (
 			<div className="header-top__blocks">
-				<div className="header-top__block">
+				<div
+					className={['header-top__block', storeActive ? 'active' : ''].join(' ')}
+					onClick={click(this.handleStoreBlockToggle, !storeActive)}
+					onMouseEnter={click(this.handleStoreBlockToggle, true)}
+					onMouseLeave={click(this.handleStoreBlockToggle, false)}
+				>
 					<span key="storeBlockIcon" className="icon">
 						<i className="fa fa-map-o"/>
 					</span>
@@ -86,10 +101,15 @@ export default class Selector extends Component {
 							{/* eslint-enable react/no-danger */}
 						</div>
 						<div className="content__actions">
-							<a href={currentLocation.get('directions')} target="_blank">Get Directions</a>
+							<a href={currentLocation.get('directions')} target="_blank">
+								Get Directions
+							</a>
 						</div>
 					</span>
 					<div className="dropdown store-dropdown" style={{maxWidth}}>
+						<div className="header-top__block__close" onClick={click(this.handleStoreBlockToggle, false)}>
+							<span className="fa fa-close"/>
+						</div>
 						<div className="header-top__stores">
 							{locations.map(location => {
 								return (
@@ -121,10 +141,7 @@ export default class Selector extends Component {
 												{/* eslint-enable react/no-danger */}
 											</div>
 											<div className="store-info__footer">
-												<a
-													onClick={click(this.handleLocationUpdate, location)}
-													className="btn btn-primary btn-sm"
-												>
+												<a onClick={click(this.handleLocationUpdate, location)} className="btn btn-primary btn-sm">
 													Set As My Store
 												</a>
 											</div>
@@ -169,13 +186,12 @@ export default class Selector extends Component {
 								<i className="fa fa-map-o"/>
 							</div>
 							{/* eslint-disable react/no-danger */}
-							<div
-								dangerouslySetInnerHTML={innerHtml(currentLocation.get('address'))}
-								className="content"
-							/>
+							<div dangerouslySetInnerHTML={innerHtml(currentLocation.get('address'))} className="content"/>
 							{/* eslint-enable react/no-danger */}
 						</div>
-						<a href={currentLocation.get('directions')} target="_blank" className="btn btn-primary btn-xs">Get Directions</a>
+						<a href={currentLocation.get('directions')} target="_blank" className="btn btn-primary btn-xs">
+							Get Directions
+						</a>
 					</div>
 					<div className="store-block phone">
 						<div className="store-block__inner">
@@ -183,12 +199,11 @@ export default class Selector extends Component {
 								<i className="fa fa-phone"/>
 							</div>
 							{/* eslint-disable react/no-danger */}
-							<div
-								dangerouslySetInnerHTML={innerHtml(currentLocation.get('phone'))}
-								className="content"
-							/>
+							<div dangerouslySetInnerHTML={innerHtml(currentLocation.get('phone'))} className="content"/>
 							{/* eslint-enable react/no-danger */}
-							<a href={phoneLink(currentLocation.get('phone'))} className="btn btn-primary btn-xs">Call Now</a>
+							<a href={phoneLink(currentLocation.get('phone'))} className="btn btn-primary btn-xs">
+								Call Now
+							</a>
 						</div>
 					</div>
 				</div>
@@ -200,16 +215,10 @@ export default class Selector extends Component {
 								<li key={location.get('id')} className="other-store">
 									<h5>{location.get('title')}</h5>
 									{/* eslint-disable react/no-danger */}
-									<div
-										dangerouslySetInnerHTML={innerHtml(currentLocation.get('address'))}
-										className="address"
-									/>
+									<div dangerouslySetInnerHTML={innerHtml(currentLocation.get('address'))} className="address"/>
 									{/* eslint-enable react/no-danger */}
 									<p className="phone">{currentLocation.get('phone')}</p>
-									<a
-										onClick={click(this.handleLocationUpdate, location)}
-										className="btn btn-primary-outline btn-xs"
-									>
+									<a onClick={click(this.handleLocationUpdate, location)} className="btn btn-primary-outline btn-xs">
 										Set As My Store
 									</a>
 								</li>
